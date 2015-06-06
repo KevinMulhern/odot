c52a6651f845beeec0ba06c4bc2da23208501169
KevinMulhern <kevinmulhern23@gmail.com>
2015-06-06 16:27:51 +0100

    Initial commit

---
+++ spec/factories.rb
@@ -0,0 +1,14 @@
+FactoryGirl.define do
+  factory :user do
+    first_name  "First"
+    last_name   "Last"
+    sequence(:email) { |n| "user#{n}@odot.com" }
+    password    "treehouse1"
+    password_confirmation "treehouse1"
+  end
+
+  factory :todo_list do
+    title  "Todo List Title"
+    user
+  end
+end
